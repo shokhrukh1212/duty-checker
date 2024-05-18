5ad67d931023a5429f377b7760894e49d7950261
Shokhrukh Karimov <shohruxkarimov546@gmail.com>
2024-05-18 22:46:02 +0500

    fix: add usernames, remove comments

--- bot.js
+++ bot.js
@@ -1,26 +1,25 @@
-require('dotenv').config();
-import TelegramBot from 'node-telegram-bot-api';
-import cron from 'node-cron';
+const dotenv = require('dotenv');
+dotenv.config();
+const TelegramBot = require('node-telegram-bot-api');
+const cron = require('node-cron');
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
-const groupId = process.env.TELEGRAM_GROUP_ID;  // Replace with your group chat ID
-const users = ['@person1', '@person2'];  // List of usernames
+const groupId = process.env.TELEGRAM_GROUP_ID;
+const users = ['@Javlon084', '@coder_naimov', '@hakimjonov712', '@Oybek_Makhsudov', '@NamozOtabek', 'Otabekov Ro"zimurod', '@aziz_ismoilov', 'Bekzod Rahmonov', '@khon_engineer'];
 
 let currentIndex = 0;
 
 cron.schedule('0 10 * * *', () => {
-    // This will run every day at 10:00 AM server time
     const username = users[currentIndex];
     bot.sendMessage(groupId, `Iltimos, kunlik navbatchilikni unutmang: ${username}`);
     
-    // Update the index for the next day
     currentIndex = (currentIndex + 1) % users.length;
 }, {
-    timezone: "Asia/Tashkent"  // Set your timezone, e.g., "America/New_York"
+    timezone: "Asia/Tashkent"
 });
 
 bot.on('polling_error', (error) => {
-    console.log(error);  // Log polling errors
+    console.log(error);
 });
